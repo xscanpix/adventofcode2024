c649e270b36d078f36bb4459d10a773c2f3d9fe6
Svante Nilsson <code@svantenilsson.dev>
2024-12-07 14:53:49 -0700

    Solve day 5 part 2

--- pkg/day5/day5.go
+++ pkg/day5/day5.go
@@ -1,34 +1,32 @@
 package day5
 
-func Solve1(input Input) int {
-	safe_updates := make([]bool, len(input.Updates))
-	for i := range safe_updates {
-		safe_updates[i] = true
+func findErrorIndexes(update []int, rulesMap map[int][]int) []int {
+	seen_before := map[int]int{}
+
+	for numberIndex, number := range update {
+		rules := rulesMap[number]
+		seen_before[number] = numberIndex
+
+		for _, rule := range rules {
+			val, ok := seen_before[rule]
+
+			if ok {
+				return []int{val, numberIndex}
+			}
+		}
 	}
 
+	return nil
+}
+
+func Solve1(input Input) int {
 	sum := 0
 
-	for updateIndex, update := range input.Updates {
-		seen_before := map[int]bool{}
+	for _, update := range input.Updates {
+		errorIndexes := findErrorIndexes(update, input.Rules)
 
-		for i, number := range update {
-			rules := input.Rules[number]
-			seen_before[number] = true
-
-			for _, rule := range rules {
-				if seen_before[rule] {
-					safe_updates[updateIndex] = false
-					break
-				}
-			}
-
-			if !safe_updates[updateIndex] {
-				break
-			}
-
-			if i == len(update)-1 {
-				sum += update[len(update)/2]
-			}
+		if errorIndexes == nil {
+			sum += update[len(update)/2]
 		}
 	}
 
@@ -36,5 +34,27 @@ func Solve1(input Input) int {
 }
 
 func Solve2(input Input) int {
-	return 0
+	sum := 0
+
+	for _, update := range input.Updates {
+		errorIndexes := findErrorIndexes(update, input.Rules)
+
+		if errorIndexes != nil {
+			for {
+				temp := update[errorIndexes[0]]
+				update[errorIndexes[0]] = update[errorIndexes[1]]
+				update[errorIndexes[1]] = temp
+
+				errorIndexes = findErrorIndexes(update, input.Rules)
+
+				if errorIndexes == nil {
+					break
+				}
+			}
+
+			sum += update[len(update)/2]
+		}
+	}
+
+	return sum
 }
