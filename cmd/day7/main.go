21fd58437845417554f232e19a33de0c0fda74ba
Svante Nilsson <code@svantenilsson.dev>
2024-12-10 12:05:22 -0700

    Solve day 7 part 2

--- cmd/day7/main.go
+++ cmd/day7/main.go
@@ -8,4 +8,5 @@ import (
 
 func main() {
 	log.Printf(`Day7 result 1: %d`, day7.Solve1(day7.ReadInputFromFile("input.txt")))
+	log.Printf(`Day7 result 2: %d`, day7.Solve2(day7.ReadInputFromFile("input.txt")))
 }
--- pkg/day7/day7.go
+++ pkg/day7/day7.go
@@ -1,5 +1,9 @@
 package day7
 
+import (
+	"math"
+)
+
 func check(line ParsedLine) int {
 	to_check := []ParsedLine{line}
 
@@ -8,6 +12,10 @@ func check(line ParsedLine) int {
 	for len(to_check) > 0 {
 		currentLine := to_check[0]
 		to_check = to_check[1:]
+
+		if len(currentLine.Numbers) <= 1 {
+			continue
+		}
 
 		currentSum := currentLine.Sum
 		for i := len(currentLine.Numbers) - 1; i > 0; i-- {
@@ -44,6 +52,36 @@ func Solve1(input Input) int {
 	return sum
 }
 
+func recurse(sum int, numbers []int) bool {
+
+	if len(numbers) == 0 {
+		panic("Impossible condition")
+	}
+
+	if len(numbers) == 1 {
+		return sum == numbers[0]
+	}
+
+	num := numbers[len(numbers)-1]
+
+	add := ((sum >= num) && recurse(sum-num, numbers[:len(numbers)-1]))
+	mul := ((sum%num) == 0 && recurse(sum/num, numbers[:len(numbers)-1]))
+
+	p := int(math.Pow10(int(math.Log10(float64(num)) + 1)))
+
+	conc := (((sum % p) == num) && recurse(sum/p, numbers[:len(numbers)-1]))
+
+	return add || mul || conc
+}
+
 func Solve2(input Input) int {
-	return 0
+	sum := 0
+
+	for _, line := range input.Lines {
+		if recurse(line.Sum, line.Numbers) {
+			sum += line.Sum
+		}
+	}
+
+	return sum
 }
