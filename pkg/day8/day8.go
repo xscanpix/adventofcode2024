d632b6c0a5e9d0c07f87f8a2b139771ac0ee473a
Svante Nilsson <code@svantenilsson.dev>
2024-12-10 14:14:17 -0700

    Solve day 8 part 2

--- pkg/day8/day8.go
+++ pkg/day8/day8.go
@@ -4,6 +4,26 @@ import (
 	"github.com/xscanpix/adventofcode2024/internal/pair"
 )
 
+func find_antinodes(antenna1 pair.Pair, antenna2 pair.Pair, distance int, gridDim pair.Pair) []pair.Pair {
+	nodes := make([]pair.Pair, 0)
+
+	xDiff := antenna1.X() - antenna2.X()
+	yDiff := antenna1.Y() - antenna2.Y()
+
+	antinode1 := pair.NewPair(antenna1.X()+(xDiff*distance), antenna1.Y()+(yDiff*distance))
+	antinode2 := pair.NewPair(antenna2.X()-(xDiff*distance), antenna2.Y()-(yDiff*distance))
+
+	if antinode1.Bounds(pair.Zero(), gridDim) {
+		nodes = append(nodes, antinode1)
+	}
+
+	if antinode2.Bounds(pair.Zero(), gridDim) {
+		nodes = append(nodes, antinode2)
+	}
+
+	return nodes
+}
+
 func Solve1(input Input) int {
 	unique_antinodes := make(map[int]bool)
 
@@ -21,18 +41,8 @@ func Solve1(input Input) int {
 				antenna1 := v[i]
 				antenna2 := v[j]
 
-				xDiff := antenna1.X() - antenna2.X()
-				yDiff := antenna1.Y() - antenna2.Y()
-
-				antinode1 := pair.NewPair(antenna1.X()+xDiff, antenna1.Y()+yDiff)
-				antinode2 := pair.NewPair(antenna2.X()-xDiff, antenna2.Y()-yDiff)
-
-				if antinode1.Bounds(pair.Zero(), input.GridDim) {
-					unique_antinodes[antinode1.Hash()] = true
-				}
-
-				if antinode2.Bounds(pair.Zero(), input.GridDim) {
-					unique_antinodes[antinode2.Hash()] = true
+				for _, node := range find_antinodes(antenna1, antenna2, 1, input.GridDim) {
+					unique_antinodes[node.Hash()] = true
 				}
 			}
 		}
@@ -42,5 +52,39 @@ func Solve1(input Input) int {
 }
 
 func Solve2(input Input) int {
-	return 0
+	unique_antinodes := make(map[int]bool)
+
+	for _, v := range *input.AntennaMap {
+		if len(v) < 2 {
+			continue
+		}
+
+		for i := range len(v) {
+			for j := range len(v) {
+				if i == j {
+					continue
+				}
+
+				antenna1 := v[i]
+				antenna2 := v[j]
+
+				distance := 0
+				for {
+					antinodes := find_antinodes(antenna1, antenna2, distance, input.GridDim)
+
+					if len(antinodes) == 0 {
+						break
+					}
+
+					for _, node := range antinodes {
+						unique_antinodes[node.Hash()] = true
+					}
+
+					distance++
+				}
+			}
+		}
+	}
+
+	return len(unique_antinodes)
 }
--- pkg/day8/day8_test.go
+++ pkg/day8/day8_test.go
@@ -13,7 +13,7 @@ func TestSolve1(t *testing.T) {
 }
 
 func TestSolve2(t *testing.T) {
-	expect := 11387
+	expect := 34
 
 	if result := Solve2(ReadInputFromFile("test_input.txt")); result != expect {
 		t.Fatalf(`Expected %d, got %d`, expect, result)
