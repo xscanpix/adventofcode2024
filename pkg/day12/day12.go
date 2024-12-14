617a70981623a6b61861e90807453776af7accc5
Svante Nilsson <code@svantenilsson.dev>
2024-12-14 15:00:07 -0700

    Solve day 12 part 2 (very brute-force)

--- pkg/day12/day12.go
+++ pkg/day12/day12.go
@@ -68,6 +68,119 @@ func Solve1(input Input) int {
 	return sum
 }
 
+func part2(input Input, regionId byte, pos pair.Pair) (int, int) {
+	current := input.Grid[pos.Y()][pos.X()]
+
+	if current.Visited || current.Id != regionId {
+		return 0, 0
+	}
+
+	input.Grid[pos.Y()][pos.X()].Visited = true
+
+	adj := nextPairs(pos, input.Width, input.Height)
+
+	corners := 0
+	areaCount := 1
+	perimeter := 4 - len(adj)
+
+	left := byte(0)
+	right := byte(0)
+	up := byte(0)
+	down := byte(0)
+
+	upleft := byte(0)
+	upright := byte(0)
+	downleft := byte(0)
+	downright := byte(0)
+
+	if pos.X() > 0 {
+		left = input.Grid[pos.Y()][pos.X()-1].Id
+	}
+
+	if pos.X() < input.Width-1 {
+		right = input.Grid[pos.Y()][pos.X()+1].Id
+	}
+
+	if pos.Y() > 0 {
+		up = input.Grid[pos.Y()-1][pos.X()].Id
+		if pos.X() > 0 {
+			upleft = input.Grid[pos.Y()-1][pos.X()-1].Id
+		}
+
+		if pos.X() < input.Width-1 {
+			upright = input.Grid[pos.Y()-1][pos.X()+1].Id
+		}
+	}
+
+	if pos.Y() < input.Height-1 {
+		down = input.Grid[pos.Y()+1][pos.X()].Id
+		if pos.X() > 0 {
+			downleft = input.Grid[pos.Y()+1][pos.X()-1].Id
+		}
+
+		if pos.X() < input.Width-1 {
+			downright = input.Grid[pos.Y()+1][pos.X()+1].Id
+		}
+	}
+
+	// Outside corners
+	if left != regionId && up != regionId {
+		corners++
+	}
+
+	if left != regionId && down != regionId {
+		corners++
+	}
+
+	if up != regionId && right != regionId {
+		corners++
+	}
+
+	if right != regionId && down != regionId {
+		corners++
+	}
+
+	// Inside corners
+	if right == regionId && down == regionId && downright != regionId {
+		corners++
+	}
+
+	if right == regionId && up == regionId && upright != regionId {
+		corners++
+	}
+
+	if left == regionId && up == regionId && upleft != regionId {
+		corners++
+	}
+
+	if left == regionId && down == regionId && downleft != regionId {
+		corners++
+	}
+
+	for _, pos := range adj {
+		if input.Grid[pos.Y()][pos.X()].Id != regionId {
+			perimeter++
+		}
+		a, c := part2(input, regionId, pos)
+		corners += c
+		areaCount += a
+	}
+
+	return areaCount, corners
+}
+
 func Solve2(input Input) int {
-	return 0
+	sum := 0
+
+	for row := range input.Height {
+		for col := range input.Width {
+			current := pair.NewPair(col, row)
+			if !input.Grid[row][col].Visited {
+				a, c := part2(input, input.Grid[row][col].Id, current)
+				sum += a * c
+			}
+		}
+	}
+
+	return sum
 }
